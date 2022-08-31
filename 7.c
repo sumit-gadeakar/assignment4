a6649923a441039c0ccbb28f5e207721cc756d6e
sumit gadekar <109942219+sumit-gadeakar@users.noreply.github.com>
2022-08-31 18:29:28 +0530

    Add files via upload

---
+++ 7.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a=1;
+    char b='z';
+    float c=2.33;
+    printf("the value of a is %d\n the value of b is %c\n the value of c is %f",a,b,c);
+    return 0;
+}
